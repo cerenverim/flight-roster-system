207d149dbf3ef12630188738bc697febaf4417d1
remzis28178 <remzis@sabanciuniv.edu>
2024-05-03 14:10:03 +0300

    exhibiting flight details function with extensive crew added

--- backend/demo/flightdemo/views.py
+++ backend/demo/flightdemo/views.py
@@ -10,3 +10,10 @@ def home(request):
 def list_flights(request):
     flights = Flight.objects.all().select_related('flight_roster', 'vehicle_type')
     return render(request, 'flights/list.html', {'flights': flights})
+
+# reviewing flight details
+def flight_details(request, flight_number):
+    flight = get_object_or_404(Flight, flight_number=flight_number)
+    crew = flight.flight_roster.flight_crew_senior.all()  # Extend to other crew as needed
+    passengers = flight.flight_roster.flight_passengers.all()
+    return render(request, 'flights/detail.html', {'flight': flight, 'crew': crew, 'passengers': passengers})
