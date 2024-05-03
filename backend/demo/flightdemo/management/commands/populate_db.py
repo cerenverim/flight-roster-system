843e340043f6fc24da342ff7c03d0df819f4e547
SezerEnisBirgili <enis.birgili@sabanciuniv.edu>
2024-05-03 17:31:32 +0300

    - roster generator function for populate_db command - added functionality: - py manage.py populate_db Roster [Count]

--- backend/demo/flightdemo/management/commands/populate_db.py
+++ backend/demo/flightdemo/management/commands/populate_db.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 
 from django.core.management.base import BaseCommand
-from flightdemo.models import Passenger, FlightCrew, CabinCrew, Dish # if there is error here, try to run .py anyway
+from flightdemo.models import Passenger, FlightCrew, CabinCrew, Dish, Roster # if there is error here, try to run .py anyway
 from faker import Faker
 from faker_food import FoodProvider
 import random
@@ -9,7 +9,7 @@ import random
 
 class Command(BaseCommand):
 
-    def generate_passengers(self, count=100, flight_number="0001"):
+    def generate_passengers(self, count=100):
 
         fake = Faker()
         temp = []
@@ -21,7 +21,6 @@ class Command(BaseCommand):
                       "age"          : fake.random_int(min=0, max=80),
                       "gender"       : choice[2],
                       "nationality"  : fake.country(),
-                      "flight_number": ("FL" + flight_number),
                       "seat_type"    : random.choice(["economy", "business"])}
 
             temp.append(person)
@@ -33,8 +32,7 @@ class Command(BaseCommand):
                                   age=x["age"],
                                   gender=x["gender"],
                                   nationality=x["nationality"],
-                                  seat_type=x["seat_type"],
-                                  flight_number=x["flight_number"])
+                                  seat_type=x["seat_type"])
 
             passenger.save()
 
@@ -179,10 +177,95 @@ class Command(BaseCommand):
 
             dish.save()
 
+    def generate_Roster(self, count=10):
+
+        # make sure there are enough persons to fill a maximum roster
+        # a maximum roster must have:
+        #   - 1 senior pilot,
+        #   - 1 junior pilot,
+        #   - 2 trainee pilot,
+        #   - 4 senior attendent,
+        #   - 16 junior attendent,
+        #   - 2 chef
+
+        fake = Faker()
+
+        senior_pilots = FlightCrew.objects.all().filter(seniority="Senior")
+        senior_pilots = list(senior_pilots)
+
+        junior_pilots = FlightCrew.objects.all().filter(seniority="Junior")
+        junior_pilots = list(junior_pilots)
+
+        trainee_pilots = FlightCrew.objects.all().filter(seniority="Trainee")
+        trainee_pilots = list(trainee_pilots)
+
+        chefs = CabinCrew.objects.all().filter(attendant_type="Chef")
+        chefs = list(chefs)
+
+        senior_cabin_crew = CabinCrew.objects.all().filter(attendant_type="Senior")
+        senior_cabin_crew = list(senior_cabin_crew)
+
+        junior_cabin_crew = CabinCrew.objects.all().filter(attendant_type="Junior")
+        junior_cabin_crew = list(junior_cabin_crew)
+
+        passengers = Passenger.objects.all()
+        passengers = list(passengers)
+
+        for i in range(count):
+
+            roster_senior_pilot = random.choice(senior_pilots)
+            roster_junior_pilot = random.choice(junior_pilots)
+
+            number_of_trainees = fake.random_int(min=0, max=2)
+            roster_trainees = fake.random_elements(trainee_pilots, length=number_of_trainees, unique=True)
+
+            number_of_seniors = fake.random_int(min=1, max=4)
+            roster_seniors = fake.random_elements(senior_cabin_crew, length=number_of_seniors, unique=True)
+
+            number_of_juniors = fake.random_int(min=4, max=16)
+            roster_juniors = fake.random_elements(junior_cabin_crew, length=number_of_juniors, unique=True)
+
+            number_of_chefs = fake.random_int(min=0, max=2)
+            roster_chefs = fake.random_elements(chefs, length=number_of_chefs, unique=True)
+
+            # the choice is between plane capacities (accounting for parents)
+            number_of_passengers = random.choice([90, 180, 270])
+            roster_passengers = fake.random_elements(passengers, length=number_of_passengers, unique=True)
+
+            for passenger in roster_passengers:
+
+                if passenger.age <= 2:
+                    parent1_ID = passenger.seat_type[0]
+                    parent2_ID = passenger.seat_type[1]
+
+                    parent1 = Passenger.objects.get(pk=parent1_ID)
+                    parent2 = Passenger.objects.get(pk=parent2_ID)
+
+                    if parent1 not in roster_passengers:
+                        roster_passengers.append(parent1)
+
+                    if parent2 not in roster_passengers:
+                        roster_passengers.append(parent2)
+
+            random.shuffle(roster_passengers)
+
+            roster = Roster(flight_crew_junior=roster_junior_pilot,
+                            flight_crew_senior=roster_senior_pilot)
+            roster.save()
+
+            roster.flight_crew_trainee.set(roster_trainees)
+            roster.flight_cabin_crew_junior.set(roster_juniors)
+            roster.flight_cabin_crew_senior.set(roster_seniors)
+            roster.flight_cabin_crew_chef.set(roster_chefs)
+            roster.flight_passengers.set(roster_passengers)
+
+            roster.save()
+
     def add_arguments(self, parser):
         parser.add_argument('persons')
         parser.add_argument('count', type=int, nargs='?', default=100)
-        parser.add_argument('flight_number', nargs='?', default="0001")
+
+        # parser.add_argument('flight_number', nargs='?', default="0001")
 
         # WIP
         # parser.add_argument('id_start', type=int, nargs='?', default=0)
@@ -190,7 +273,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         if options["persons"] == "Passenger":
-            self.generate_passengers(options["count"], options["flight_number"])
+            self.generate_passengers(options["count"])
 
         elif options["persons"] == "FlightCrew":
             self.generate_flightCrew(options["count"])
@@ -200,6 +283,9 @@ class Command(BaseCommand):
 
         elif options["persons"] == "Dish":
             self.generate_Dish()
+
+        elif options["persons"] == "Roster":
+            self.generate_Roster(options["count"])
 
 
 
