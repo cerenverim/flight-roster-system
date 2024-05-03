1da2773e9bb0ed34cfe6de9844ec08c50ac97688
remzis28178 <remzis@sabanciuniv.edu>
2024-05-03 14:07:14 +0300

    Add view function list_flights to retrieve and display a list of all flights

--- backend/demo/flightdemo/views.py
+++ backend/demo/flightdemo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Flight, Passenger
 
 # Create your views here.
 def home(request):
     return HttpResponse("Backend Demo")
+
+# Listing all of the flights 
+def list_flights(request):
+    flights = Flight.objects.all().select_related('flight_roster', 'vehicle_type')
+    return render(request, 'flights/list.html', {'flights': flights})
