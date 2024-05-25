2a14cffcd8f7dfd6151272fc44ed0c1b48aa77ed
SavOza <117209961+SavOza@users.noreply.github.com>
2024-05-25 19:11:17 +0300

    Added tests for cabin crew and pilots

--- backend/demo/flight_crew_information/tests.py
+++ backend/demo/flight_crew_information/tests.py
@@ -1,3 +1,78 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from .models import FlightCrew
+from flight_information.models import VehicleType
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class PilotTests(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        token, created = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+
+        self.vehicle_type = VehicleType.objects.create(
+            vehicle_name='Boeing 747',
+            vehicle_crew_capacity=10,
+            vehicle_pilot_capacity=2,
+            vehicle_passenger_capacity=300,
+            vehicle_business_seats=20,
+            vehicle_seating_plan=3
+        )
+
+        self.vehicle_type2 = VehicleType.objects.create(
+            vehicle_name='Supreme Vehicle',
+            vehicle_crew_capacity=10,
+            vehicle_pilot_capacity=2,
+            vehicle_passenger_capacity=300,
+            vehicle_business_seats=20,
+            vehicle_seating_plan=3
+        )
+
+        self.pilot1 = FlightCrew.objects.create(
+            name='John Doe',
+            age=30,
+            gender='Male',
+            nationality='US',
+            languages=['English', 'Spanish'],
+            vehicle=self.vehicle_type,
+            max_range=1000,
+            seniority=1
+        )
+
+        self.pilot2 = FlightCrew.objects.create(
+            name='Man Doe',
+            age=30,
+            gender='Male',
+            nationality='US',
+            languages=['English', 'Spanish'],
+            vehicle=self.vehicle_type2,
+            max_range=1000,
+            seniority=2
+        )
+
+
+    def test_get_pilot_by_vehicle_type_valid(self):
+        url = reverse('get_pilot_by_vehicle_type', args=[self.vehicle_type.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], 'John Doe')
+
+    def test_get_pilot_by_vehicle_type_invalid(self):
+        url = '/api/pilots_api/pilots/a/'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_pilot_by_vehicle_type_nonexistant(self):
+        url = '/api/pilot_api/pilots/9/'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
--- backend/demo/flight_information/tests.py
+++ backend/demo/flight_information/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+# tests.py
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from .models import Flight, Location, VehicleType
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+
+class FlightTests(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        token, created = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        self.location1 = Location.objects.create(country='USA', city='New York', airport_name='JFK', airport_code='JFK')
+        self.location2 = Location.objects.create(country='UK', city='London', airport_name='Heathrow', airport_code='LHR')
+
+        self.vehicle_type = VehicleType.objects.create(
+            vehicle_name='Boeing 747',
+            vehicle_crew_capacity=10,
+            vehicle_pilot_capacity=2,
+            vehicle_passenger_capacity=300,
+            vehicle_business_seats=20,
+            vehicle_seating_plan=3
+        )
+
+        self.flight1 = Flight.objects.create(
+            flight_number='AA123',
+            flight_trainee_limit=2,
+            flight_date='2024-05-22T11:22:31Z',
+            flight_duration='01:00:00',
+            flight_distance=500,
+            flight_src=self.location1,
+            flight_dest=self.location2,
+            vehicle_type=self.vehicle_type,
+        )
+
+        self.flight2 = Flight.objects.create(
+            flight_number='AA124',
+            flight_trainee_limit=3,
+            flight_date='2024-05-22T12:22:31Z',
+            flight_duration='02:00:00',
+            flight_distance=1000,
+            flight_src=self.location2,
+            flight_dest=self.location1,
+            vehicle_type=self.vehicle_type,
+        )
+
+    def test_get_all_flights(self):
+        url = reverse('list_flights')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['flight_number'], 'AA123')
+        self.assertEqual(response.data[1]['flight_number'], 'AA124')
+
+    def test_get_single_flight(self):
+        url = reverse('flight_details', args=['AA123'])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['flight_number'], 'AA123')
+        self.assertEqual(response.data['flight_trainee_limit'], 2)
+        self.assertEqual(response.data['flight_date'], '2024-05-22T11:22:31Z')
+        self.assertEqual(response.data['flight_duration'], '01:00:00')
+        self.assertEqual(response.data['flight_distance'], 500)
+        self.assertEqual(response.data['flight_src'], self.location1.airport_code)
+        self.assertEqual(response.data['flight_dest'], self.location2.airport_code)
+        self.assertEqual(response.data['vehicle_type'], self.vehicle_type.id)
+
+    def test_get_single_flight_nonexistant(self):
+        url = reverse('flight_details', args=['CC999'])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
