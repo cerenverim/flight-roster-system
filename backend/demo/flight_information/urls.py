6cbf4087c7d82900508a9bd430989dad842f1df5
Barkın Var <varbarkn@gmail.com>
2024-05-26 13:03:45 +0300

    delete roster

--- backend/demo/flight_information/urls.py
+++ backend/demo/flight_information/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('flights/<str:flight_number>/', views.FlightDetailsView.as_view(), name='flight_details'),
     path('flights/<str:flight_id>/roster/', views.get_roster_by_flight, name='get_roster_by_flight'),
     path('flights/<str:flight_number>/auto_generate_roster/', views.auto_generate_roster, name='auto_generate_roster'),
+    path('flights/<str:flight_number>/delete_roster/', views.delete_roster, name='delete_roster'),
 ]
--- backend/demo/flight_information/views.py
+++ backend/demo/flight_information/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
@@ -10,7 +9,7 @@ from cabin_crew_information.models import CabinCrew
 from passenger_information.serializers import PassengerSerializer
 from .models import Flight, Roster
 from flight_crew_information.models import FlightCrew
-from .serializers import FlightSerializer, RosterSerializer, RosterSerializer
+from .serializers import FlightSerializer, RosterSerializer
 from rest_framework.decorators import api_view
 from passenger_information.models import Passenger, PlacedPassenger
 import random, math
@@ -22,7 +21,7 @@ class ListFlightsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        flights = Flight.objects.all().select_related('flight_roster', 'vehicle_type').order_by('flight_number')
+        flights = Flight.objects.all().select_related('flight_roster', 'vehicle_type')
         serializer = FlightSerializer(flights, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -35,24 +34,14 @@ class FlightDetailsView(APIView):
         flight = get_object_or_404(Flight, flight_number=flight_number)
         serializer = FlightSerializer(flight)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def get_roster_by_flight(request, flight_id):
-    try:
-        flight = Flight.objects.get(flight_number=flight_id)
-    except Flight.DoesNotExist:
-        return Response({"detail": "Flight not found."}, status=status.HTTP_404_NOT_FOUND)
-
-    if flight.flight_roster is None:
-        return Response({"detail": "Roster not found."}, status=status.HTTP_404_NOT_FOUND)
-
-    roster = flight.flight_roster
-    serializer = RosterSerializer(roster)
-    return Response(serializer.data, status=status.HTTP_200_OK)
     
+
+# Viewing the flight details
+class RosterView(APIView):
+    def get(self, request, flight_number, *args, **kwargs):
+        flight = get_object_or_404(Flight, flight_number=flight_number)
+        serializer = RosterSerializer(flight.flight_roster)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 def assign_seats_helper(flight, flight_number, type):
     
@@ -287,3 +276,28 @@ def auto_generate_roster(request, flight_number):
 
     serializer = RosterSerializer(roster)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def delete_roster(request, flight_number):
+    flight = get_object_or_404(Flight, flight_number=flight_number)
+    if flight.flight_roster == None:
+        return Response({"message":"No such roster!"}, status=status.HTTP_200_OK)
+    
+    
+    placed_passengers = flight.flight_roster.flight_passengers.all()
+    
+    # Remove the placed passengers from the roster relationship
+    
+    placed_passenger_ids = placed_passengers.values_list('id', flat=True)
+    PlacedPassenger.objects.filter(id__in=placed_passenger_ids).delete()
+
+    flight.flight_roster.delete()
+    
+    flight.flight_roster = None
+    flight.save()
+
+    # Delete the placed passengers from the database
+
+    return Response({"message":"Succesfully deleted roaster"}, status=status.HTTP_200_OK)
