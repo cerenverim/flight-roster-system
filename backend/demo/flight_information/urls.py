5bbb6cd4e298a39c85f86846476aa2dc9009aab0
Barkın Var <varbarkn@gmail.com>
2024-05-26 12:25:02 +0300

    fiix url path

--- backend/demo/flight_information/urls.py
+++ backend/demo/flight_information/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('flights/', views.ListFlightsView.as_view(), name='list_flights'),
     path('flights/<str:flight_number>/', views.FlightDetailsView.as_view(), name='flight_details'),
     path('flights/<str:flight_id>/roster/', views.get_roster_by_flight, name='get_roster_by_flight'),
-    path('flights/<str:flight_number>/auto_generate_roster', views.auto_generate_roster, name='generate_roster_automatically'),
+    path('flights/<str:flight_number>/auto_generate_roster/', views.auto_generate_roster, name='auto_generate_roster'),
 ]
